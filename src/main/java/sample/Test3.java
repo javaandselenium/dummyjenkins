cc76fff8e338415f65719cd1444efa72d3b1fba8
QSP <QSP@DESKTOP-EVK9GMC>
2021-02-12 20:15:53 +0530

    Hello1

---
+++ src/main/java/sample/Test3.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void demo() {
+		Reporter.log("Hello python",true);
+	}
+
+}
---
+++ src/main/java/sample/Test4.java
@@ -0,0 +1,12 @@
+package sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test4 {
+	@Test
+	public void demo() {
+		Reporter.log("Hello Appium",true);
+	}
+
+}
